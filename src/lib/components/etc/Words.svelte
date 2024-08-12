9cbeb1ade180630b52107b55a14af0989c2e8f75
anchannean <ann.channean@gmail.com>
2024-08-12 23:05:34 +0700

    add words update

--- src/lib/components/etc/Words.svelte
+++ src/lib/components/etc/Words.svelte
@@ -2,6 +2,7 @@
 	import { enhance } from '$app/forms';
 	import SubmitButton from '$lib/components/etc/SubmitButton.svelte';
 	import { t } from '$lib/translations';
+	import DeleteModal from './DeleteModal.svelte';
 	interface Words {
 		id: number;
 		text: string;
@@ -13,10 +14,20 @@
 	let loading = false;
 	export let modal_name: string;
 	export let value = '';
-	
+	function handleText(text: string) {
+		if (value.includes(text.concat(','))) {
+			value = value.replace(text.concat(','), '');
+		} else {
+			value = value.concat(text).concat(',');
+		}
+	}
+	let isEdit = false;
+	let words_id: number;
+	let words_ = '';
 </script>
 
-<!-- @_List_Parameter -->
+<DeleteModal action="/opd/words/?/delete_words" id={words_id} />
+
 <div class="modal fade" id={modal_name}>
 	<div class="modal-dialog modal-dialog-scrollabl modal-xl">
 		<div class="modal-content">
@@ -36,13 +47,16 @@
 					use:enhance={() => {
 						loading = true;
 						return async ({ update }) => {
-							await update({ reset: false });
+							await update({ reset: true });
 							loading = false;
+							words_id = 0;
+							isEdit = false;
 						};
 					}}
-					action="/opd/words/?/create_words"
+					action={words_id > 0 ? '/opd/words/?/update_words' : '/opd/words/?/create_words'}
 					method="post"
 				>
+					<input type="hidden" name="id" value={words_id ?? ''} />
 					<div class="form-group pb-3">
 						<div class="row">
 							<div class="col">
@@ -51,6 +65,7 @@
 							<input type="hidden" name="type" value={modal_name} />
 							<div class="col">
 								<input
+									value={words_}
 									required
 									name="word"
 									type="text"
@@ -65,19 +80,45 @@
 					</div>
 				</form>
 				<div class=" form-group row pb-3">
-					{#each find_words as item (item.id)}
+					{#each find_words as item}
 						{#if item.text}
-							<div class=" col-3 p-2">
-								<div class="form-check">
-									<input
-										bind:group={value}
-										class="form-check-input"
-										type="checkbox"
-										id={item.id.toString()}
-										value={item.text}
-									/>
-									<label for={item.id.toString()} class="custom-control-label">{item.text}</label>
-								</div>
+							<div class="col-3 p-2">
+								<input
+									on:click={() => {
+										handleText(item.text);
+									}}
+									checked={value.includes(item.text)}
+									class="form-check-input"
+									type="checkbox"
+									id={item.id.toString()}
+									value={item.text}
+								/>
+
+								<button
+									type="button"
+									class={words_id === item.id && isEdit
+										? 'btn btn-link m-0 p-0'
+										: 'btn btn-link text-secondary m-0 p-0'}
+									on:click={() => {
+										words_id = 0;
+										words_id = item.id;
+										isEdit = !isEdit;
+										words_ = '';
+										words_ = item.text;
+									}}><i class="fa-solid fa-file-pen"></i></button
+								>
+								<button
+									on:click={() => {
+										words_id = 0;
+										words_id = item.id;
+									}}
+									class="btn btn-link text-danger m-0 p-0"
+									data-bs-toggle="modal"
+									type="button"
+									data-bs-target="#delete_modal"><i class="fa-solid fa-x"></i></button
+								>
+
+								<label for={item.id.toString()} class="custom-control-label">{item.text}</label>
 							</div>
 						{/if}
 					{/each}
--- src/routes/(dash)/opd/words/+page.server.ts
+++ src/routes/(dash)/opd/words/+page.server.ts
@@ -1,6 +1,7 @@
 import { db } from '$lib/server/db';
 import { words } from '$lib/server/schema';
 import { logErrorMessage } from '$lib/server/telegram';
+import { eq } from 'drizzle-orm';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async () => {
@@ -17,6 +18,30 @@ export const actions: Actions = {
 				text: word,
 				type: type
 			})
+			.catch((e) => {
+				logErrorMessage(e);
+			});
+	},
+	update_words: async ({ request }) => {
+		const body = await request.formData();
+		const { word, type, id } = Object.fromEntries(body) as Record<string, string>;
+		await db
+			.update(words)
+			.set({
+				text: word,
+				type: type
+			})
+			.where(eq(words.id, +id))
+			.catch((e) => {
+				logErrorMessage(e);
+			});
+	},
+	delete_words: async ({ request }) => {
+		const body = await request.formData();
+		const { id } = Object.fromEntries(body) as Record<string, string>;
+		await db
+			.delete(words)
+			.where(eq(words.id, +id))
 			.catch((e) => {
 				logErrorMessage(e);
 			});
