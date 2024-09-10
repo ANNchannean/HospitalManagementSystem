051cf77efdf1ccdab869021d69323435ed064136
annchannean <ann.channean@gmail.com>
2024-09-10 12:13:13 +0700

    fix submit

--- src/lib/coms/SubmiteSearch.svelte
+++ src/lib/coms/SubmiteSearch.svelte
@@ -7,20 +7,21 @@
 	export let height = '300';
 	export let placeholder = '';
 	let q = '';
-	$: qq = $page.url.searchParams.get('q');
-	$: data = items.filter((el) => el.name.toLowerCase().includes(q.toLowerCase()));
-	$: {
-		if (browser) {
+	let timeout: number | NodeJS.Timeout;
+	const handleQ = () => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
 			if (data.length === 1 && !qq) {
 				document.getElementById('submit')?.click();
 			}
-		}
-	}
+		}, 400);
+	};
+	$: qq = $page.url.searchParams.get('q');
+	$: data = items.filter((el) => el.name.toLowerCase().includes(q.toLowerCase())).slice(0, 50);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 <div
 	on:click={(e) => {
@@ -30,6 +31,7 @@
 >
 	<!-- svelte-ignore a11y-autofocus -->
 	<input
+		on:input={handleQ}
 		{placeholder}
 		name="q"
 		autocomplete="off"
@@ -43,29 +45,33 @@
 	<ul style="width: 100%;" class="dropdown-menu">
 		<div style=" max-height: {height.concat('px')}; overflow-y: auto;">
 			<!-- svelte-ignore a11y-invalid-attribute -->
-			{#each data as item (item.id)}
-				<!-- svelte-ignore a11y-autofocus -->
-				<form
-					autofocus
-					method="post"
-					use:enhance={() => {
-						$globalLoading = true;
-						return async ({ update }) => {
-							q = '';
-							await update();
-							$globalLoading = false;
-							document.getElementById('dropdown')?.focus();
-						};
-					}}
-					action="?/create_product_order"
-				>
-					<li>
-						<input type="hidden" value={item.id ?? ''} name="product_id" />
-						<input type="hidden" value={item.price ?? ''} name="price" />
-						<button id="submit" type="submit" class="dropdown-item">{item.name ?? ''}</button>
-					</li>
-				</form>
-			{/each}
+			{#if data.length}
+				{#each data as item (item.id)}
+					<!-- svelte-ignore a11y-autofocus -->
+					<form
+						autofocus
+						method="post"
+						use:enhance={() => {
+							$globalLoading = true;
+							return async ({ update }) => {
+								q = '';
+								await update();
+								$globalLoading = false;
+								document.getElementById('dropdown')?.focus();
+							};
+						}}
+						action="?/create_product_order"
+					>
+						<li>
+							<input type="hidden" value={item.id ?? ''} name="product_id" />
+							<input type="hidden" value={item.price ?? ''} name="price" />
+							<button id="submit" type="submit" class="dropdown-item">{item.name ?? ''}</button>
+						</li>
+					</form>
+				{/each}
+			{:else}
+				<button type="button" class="dropdown-item">Not Found</button>
+			{/if}
 		</div>
 	</ul>
 </div>
